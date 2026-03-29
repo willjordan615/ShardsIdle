ea6018e947da3e00f2987e302fb300d4bd97fbf1
willjordan615 <will.jordan615@gmail.com>
2026-03-29 11:37:00 -0400

    Attempt 2 at Avatar Panning

--- js/character-management.js
+++ js/character-management.js
@@ -984,7 +984,7 @@ async function showCharacterDetail(characterId, opts = {}) {
                     bgEl.style.backgroundImage  = `url('${img.src}')`;
                     bgEl.style.backgroundSize   = 'auto 160%';
                     bgEl.style.backgroundRepeat = 'no-repeat';
-                    bgEl.style.backgroundPositionX = 'right';
+                    bgEl.style.backgroundPosition = 'right 20%';
                     bgEl.style.animation = 'detail-portrait-pan 8s ease-in-out infinite alternate';
                 }
             }
