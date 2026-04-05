6d82ad485736b73b41df363fa35a389f71dc4f04
willjordan615 <will.jordan615@gmail.com>
2026-04-05 02:17:34 -0400

    Combat System afterall!

--- js/combat-system.js
+++ js/combat-system.js
@@ -140,8 +140,7 @@ function toggleLore(id) {
  */
 function selectCharacterForChallenge(characterId) {
     currentState.detailCharacterId = characterId;
-    window.initMapScreen(characterId);
-    showScreen('map');
+    window.initMapScreen(characterId).then(() => showScreen('map'));
 }
 
 /**
